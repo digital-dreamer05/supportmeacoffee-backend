3ae10bb9b28d7053710eabb73083db5311e04014
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-05-20 21:33:22 +0330

    feat(feedback): add route to get all feedbacks

--- routes/feedbackRouter.js
+++ routes/feedbackRouter.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const router = express.Router();
 const submitFeedback = require("../controllers/v1/feedbackController");
+const authController = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+// router.use(authController.protect);
+
+// router.use(authController.restrictTo("admin"));
 
 router.post("/", submitFeedback.submitFeedback);
+
+router.get("/", submitFeedback.getAllFeedbacks);
 
 module.exports = router;
