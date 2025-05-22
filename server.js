6ca5f6731977149d4178feec7f9667c47c99afcf
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-05-22 13:54:43 +0330

    chore(db): connect MongoDB to Atlas cluster

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
-const path = require("path");
-const dotenv = require("dotenv");
-const app = require("./app");
+const mongoose = require('mongoose');
+const path = require('path');
+const dotenv = require('dotenv');
+const app = require('./app');
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' });
 
 const port = process.env.PORT || 4002;
 const dbUrl = process.env.DATABASE_LOCAL;
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
@@ -27,29 +27,29 @@ process.on("uncaughtException", (err) => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
-      .then(() => console.log("Connected to MongoDB Atlas 💾"))
-      .catch((err) => console.error("DB connection error ❌:", err));
+      .then(() => console.log('Connected to MongoDB Atlas 💾'))
+      .catch((err) => console.error('DB connection error ❌:', err));
 
     const server = app.listen(port, () => {
       console.log(`Server running on port ${port}...`);
     });
 
-    process.on("unhandledRejection", (err) => {
-      console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+    process.on('unhandledRejection', (err) => {
+      console.log('UNHANDLED REJECTION! 💥 Shutting down...');
       console.log(err.name, err.message);
       server.close(() => {
         process.exit(1);
       });
     });
 
-    process.on("SIGTERM", () => {
-      console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+    process.on('SIGTERM', () => {
+      console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
       server.close(() => {
-        console.log("💥 Process terminated!");
+        console.log('💥 Process terminated!');
       });
     });
   } catch (err) {
-    console.log("❌ Failed to connect to DB:", err);
+    console.log('❌ Failed to connect to DB:', err);
   }
 })();
 
